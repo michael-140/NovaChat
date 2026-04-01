f51e1ad457c7ad228983b5c66c63a1e4c99693ce
michael-140 <cmlok140@gmail.com>
2026-04-02 02:19:44 +0800

    Add the Chatbot Page with baisc css

--- NovaChatFrontend/src/App.jsx
+++ NovaChatFrontend/src/App.jsx
@@ -1,4 +1,5 @@
 import Header from './components/Header'
+import { ChatBotPage } from './pages/ChatBotPage/ChatBotPage'
 import './App.css'
 
 function App() {
@@ -6,7 +7,7 @@ function App() {
   return (
     <>
      <Header />
-
+    <ChatBotPage />
     </>
   )
 }
