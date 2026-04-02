273c451a85ecafd45eb6925e4ef4bf0fdd34bd35
michael-140 <cmlok140@gmail.com>
2026-04-02 18:57:13 +0800

    Update the reponsive content switching for sidebar

--- NovaChatFrontend/src/pages/ChatBotPage/ChatBotSideBar/ChatBotSideBar.jsx
+++ NovaChatFrontend/src/pages/ChatBotPage/ChatBotSideBar/ChatBotSideBar.jsx
@@ -1,27 +1,67 @@
 import './ChatBotSideBar.css'
 import botIcon from '../../../images/chatbotIcon.png'
+import { useState, useEffect } from 'react'
+
+
+function ChatHistory({ chatTopic }) {
+    return (
+        <div className="chat-history">
+            {/* <img className='chat-icon' src={botIcon}></img> */}
+            <div className="sidebar-chat-topic">
+                <span>{chatTopic}</span>
+            </div>
+        </div>
+    )
+}
 
 export default function ChatBotSideBar() {
+    const data = [
+        {
+            id: 1,
+            topic: "Css1Css1Css1Css1Css1",
+        },
+        {
+            id: 2,
+            topic: "css2"
+        },
+        {
+            id: 3,
+            topic: "css3"
+        }
+    ]
+
+    const [chatHistory, setchatHistory] = useState(data)
+
+
     return (
+
         <div className="chat-sidebar">
-            <div className="chat-historys">
-                <div className='chat-subtitle'>Chat History</div>
-                <div className="chat-history history-active">
-                    <img className='chat-icon' src={botIcon}></img>
-                    <div className="sidebar-chat-topic">
-                        About CSS
+            
+            <img className="menuIcon" src='/memuIcon.svg'></img>
+
+            <div className="chat-histories-container">
+                <div className='chat-subtitle'><span>Chat History</span></div>
+
+                <div className="chat-hisories">
+
+                    <div className="new-chat-history">
+                        <img className='new-chat-icon' src='/newChatIcon.svg'></img>
+                        <div className="new-sidebar-chat-topic">
+                            <span>New Chat</span>
+                        </div>
                     </div>
+
+
+                    {
+                        chatHistory.map((history) => {
+                            return <ChatHistory chatTopic={history.topic} key={history.id} />
+                        })
+                    }
                 </div>
 
-                <div className="chat-history">
-                    <img className='chat-icon' src={botIcon}></img>
-                    <div className="sidebar-chat-topic">
-                        About CSS2
-                    </div>
-                </div>
+
             </div>
 
-            <img className="menuIcon" src='/memuIcon.svg'></img>
 
 
         </div>
