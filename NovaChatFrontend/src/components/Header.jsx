2b9ac3a0ebf46d52d62cbad7bd6086550e2dee9b
michael-140 <cmlok140@gmail.com>
2026-04-03 16:51:31 +0800

    Update the Header

--- NovaChatFrontend/src/components/Header.jsx
+++ NovaChatFrontend/src/components/Header.jsx
@@ -5,17 +5,24 @@ export default function Header() {
         <header className="header">
 
             <div className="icon">
-                <img src="/novachatIcon.svg" alt="NovaChat Icon" className="icon-image" />
-                <span>NovaChat</span>
+                <a href="/home">
+                    <img src="/novachatIcon.svg" alt="NovaChat Icon" className="icon-image" />
+                </a>
+                <a href="/home">
+                    <span>NovaChat</span>
+                </a>
+
             </div>
 
             <div className="navigation">
                 <a href="/home" className="nav-link ">Home</a>
                 <a href="/chatbot" className="nav-link active">ChatBot</a>
-                <a href="contact" className="nav-link ">Contact</a>
+                <a href="/contact" className="nav-link ">Contact</a>
             </div>
             
-            <div className="login">Login</div>
+            <div href="/login" className="login">
+                <a href="/login" className="nav-link">Login</a>
+            </div>
 
         </header>
     )
